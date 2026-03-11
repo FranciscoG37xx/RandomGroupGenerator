8817902dba56d3995f8cb9026458e146767ce564
Xavier Vale <xaviervale@ua.pt>
2026-03-11 22:33:56 +0000

    Versao final da aplicacao

--- RandomGroupGeneratorLibrary/src/Main.java
+++ RandomGroupGeneratorLibrary/src/Main.java
@@ -2,28 +2,48 @@ import randomgroupgenerator.Student;
 import randomgroupgenerator.Group;
 import randomgroupgenerator.RandomGroupGenerator;
 import java.util.List;
+import java.util.ArrayList;
+import java.io.File;
+import lp.trabalho1.IODataClass;
+import lp.trabalho1.StudentInfo;
 
 public class Main {
 
     public static void main(String[] args) {
 
         RandomGroupGenerator generator = new RandomGroupGenerator();
+        IODataClass io = new IODataClass();
 
-        Student s1 = new Student("1", "Ana");
-        Student s2 = new Student("2", "Joao");
-        Student s3 = new Student("3", "Maria");
-        Student s4 = new Student("4", "Pedro");
+        StudentInfo[] students = io.loadStudentUC("students.txt");
 
-        generator.addStudent(s1);
-        generator.addStudent(s2);
-        generator.addStudent(s3);
-        generator.addStudent(s4);
+        if (students.length % 2 != 0) {
+            System.out.println("O numero de estudantes tem de ser par.");
+            return;
+        }
+
+        for (StudentInfo s : students) {
+            Student student = new Student(String.valueOf(s.getStudentID()), s.getStudentName());
+            generator.addStudent(student);
+        }
 
         List<Group> groups = generator.generateGroups();
+        List<String> output = new ArrayList<>();
+        int groupNumber = 1;
 
-        System.out.println("Grupos gerados:");
         for (Group g : groups) {
-            System.out.println(g);
+            String line = groupNumber + " - " +
+                    g.getStudent1().getName() + "(" + g.getStudent1().getId() + ")" + " e " +
+                    g.getStudent2().getName() + "(" + g.getStudent2().getId() + ")";
+
+            System.out.println(line);
+            output.add(line);
+            groupNumber++;
         }
+
+        String outputFile = "groups.txt";
+        io.outputGroups(outputFile, output.toArray(new String[0]));
+
+        File file = new File(outputFile);
+        System.out.println("Ficheiro criado em: " + file.getAbsolutePath());
     }
 }
